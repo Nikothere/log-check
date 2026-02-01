c85f552eada6ded9397728e18792486097c50e02
Nikothere <alialekber2876@gmail.com>
2026-02-01 18:14:17 +0400

    add colors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,31 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.io.IOException;
 
-public  class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
-        Path LogFile = Path.of("src/main/resources/log.log");
+        List<String> lines = Files.readAllLines(Path.of("./src/main/resources/log.log"));
 
-        List<String> lines = Files.readAllLines(LogFile);
 
         int error = 0;
+        int waring = 0;
         int info = 0;
-        int warning = 0;
-
         for (String line : lines) {
             if (line.contains("ERROR")) {
                 error++;
             }
             if (line.contains("INFO")) {
                 info++;
-
-            if (line.contains("WARNING")) {
-                warning++;}
+            }
+            if (line.contains("WARING")) {
+                waring++;
             }
         }
+        System.out.println(color.RED + "error:" + error+ color.RESET +"");
+        System.out.println(color.YELLOW + "waring:" + waring+ color.RESET +"");
+        System.out.println(color.BLUE + "info: " + info + color.RESET + "");
 
-        System.out.println("error: " + error + " ");
-        System.out.println("warning: " + warning + " ");
-        System.out.println("info: " + info + " ");
-
-        if (error == 0) {
-            System.out.println("ошибок не найдено");
-
-        }
     }
 }
