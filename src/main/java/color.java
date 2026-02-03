e733cd896582fb7240572e021396c6f5a9977fd2
Nikothere <alialekber2876@gmail.com>
2026-02-03 16:55:54 +0400

    -greeting text -reworked as a command -slightly tweaked variables -bug fixes

--- src/main/java/color.java
+++
@@ -1,7 +0,0 @@
-public class color {
-    public static final String RESET  = "\u001B[0m";
-
-    public static final String RED    = "\u001B[31m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String	BLUE  = "\u001B[34m";
-}
