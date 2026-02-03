f893333d22926736b05347789bd26f71729f0dd9
Niko <alialekber2876@gmail.com>
2026-02-03 17:01:58 +0400

    Add files via upload
    For some reason this file wasn't added, so I added it myself

---
+++ src/main/java/Ansi.java
@@ -0,0 +1,6 @@
+public final class Ansi {
+    public static final String RESET  = "\u001B[0m";
+    public static final String RED    = "\u001B[31m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE   = "\u001B[34m";
+}
